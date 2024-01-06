483a31a34a29e077f02b2f6b557fb2c5b09d2cbe
DJcomeback <1422861715@qq.com>
2024-01-06 15:50:24 +0800

    LoginController_changes

--- gec-service-system/src/main/java/com/gec/system/controller/LoginController.java
+++ gec-service-system/src/main/java/com/gec/system/controller/LoginController.java
@@ -1,4 +1,39 @@
 package com.gec.system.controller;
 
+import com.gec.system.util.Result;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@Api(tags = "登录管理控制器")
+@RequestMapping(value = "/admin/system/index")
 public class LoginController {
+
+    /**
+     *  登录接口
+     * @return
+     */
+    @PostMapping(value = "/login")
+    public Result login() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", "admin-token");
+        return Result.ok(map);
+    }
+
+    @GetMapping(value = "/info")
+    public Result info() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("roles","[admin]");
+        map.put("introduction","I am a super admin");
+        map.put("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        map.put("name","admin maXen hello!");
+        return Result.ok(map);
+    }
 }
